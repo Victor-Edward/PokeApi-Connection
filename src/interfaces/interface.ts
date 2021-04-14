5aab63dd3cb531b4f619a31ce6d5e6a69739b25d
Victor Edward <victoredward30@gmail.com>
2021-04-14 06:42:24 -0300

    feat: Created the interface to config the receive data

--- src/interfaces/interface.ts
+++ src/interfaces/interface.ts
@@ -1,3 +1,21 @@
 export interface pokemon {
     name: string;
+    types: {
+        type: {
+            name: string;
+        }
+    }[];
+    weight: number;
+    height: number;
+    id: number;
+    sprites: {
+        front_default: string;
+        back_default: string;
+    };
+    stats: {
+        base_stat: number;
+        stat: {
+            name: string;
+        }
+    }[];
 }
