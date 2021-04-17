10ef8e5c51f4372bd8fd75993f43184310007e17
Victor Edward <victoredward30@gmail.com>
2021-04-17 19:59:04 -0300

    feat: Added icons for pokemon types

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,24 @@ import React, { useState, useEffect } from 'react'
 import { pokemon } from '../interfaces/interface'
 import styles from '../styles/main.module.css'
 import { getpokemon } from '../interfaces/api'
+import normalIcon from '../assets/images/pokemon_type_icons/normal.png' 
+import bugIcon from '../assets/images/pokemon_type_icons/bug.png' 
+import darkIcon from '../assets/images/pokemon_type_icons/dark.png' 
+import dragonIcon from '../assets/images/pokemon_type_icons/dragon.png' 
+import electricIcon from '../assets/images/pokemon_type_icons/electric.png' 
+import fairyIcon from '../assets/images/pokemon_type_icons/fairy.png' 
+import fightingIcon from '../assets/images/pokemon_type_icons/fighting.png' 
+import fireIcon from '../assets/images/pokemon_type_icons/fire.png' 
+import flyingIcon from '../assets/images/pokemon_type_icons/flying.png' 
+import ghostIcon from '../assets/images/pokemon_type_icons/ghost.png' 
+import grassIcon from '../assets/images/pokemon_type_icons/grass.png' 
+import groundIcon from '../assets/images/pokemon_type_icons/ground.png' 
+import iceIcon from '../assets/images/pokemon_type_icons/ice.png' 
+import poisonIcon from '../assets/images/pokemon_type_icons/poison.png' 
+import psychicIcon from '../assets/images/pokemon_type_icons/psychic.png' 
+import rockIcon from '../assets/images/pokemon_type_icons/rock.png' 
+import steelIcon from '../assets/images/pokemon_type_icons/steel.png' 
+import waterIcon from '../assets/images/pokemon_type_icons/water.png' 
 
 const MainPage = () => {
     
@@ -39,12 +57,39 @@ const MainPage = () => {
         }
     }
 
+    const icons:any = {
+        normal: normalIcon,
+        fighting: fightingIcon,
+        flying: flyingIcon,
+        water: waterIcon,
+        fire: fireIcon,
+        electric: electricIcon,
+        ice: iceIcon,
+        grass: grassIcon,
+        bug: bugIcon,
+        poison: poisonIcon,
+        rock: rockIcon,
+        ground: groundIcon,
+        steel: steelIcon,
+        dragon: dragonIcon,
+        ghost: ghostIcon,
+        dark: darkIcon,
+        fairy: fairyIcon,
+        psychic: psychicIcon,
+    }
+
     const [displayPokemon, setDisplayPokemon] = useState <pokemon> ()
 
     const load = async (inputPokemon: string) => {
-        const loadData = await getpokemon(inputPokemon)
+        const loadData = await getpokemon(inputPokemon.toLowerCase())
         setDisplayPokemon(loadData)  
     }
+    
+    function capitalizeFirstLetter(name: any) {
+        if (name){
+            return name.charAt(0).toUpperCase() + name.slice(1);
+        }
+      }
 
     useEffect(changeColor, [displayPokemon])
 
@@ -52,41 +97,53 @@ const MainPage = () => {
         <div className={styles.container}>
             <div className={styles.greetings}>
                 <h2>Seja bem vindo ao PokeApi, para começar, digite um pokemon e pressione Enter</h2>
-                <input onKeyPress={(e:any) => {if (e.code === "Enter") {load(e.target.value)}}} type="text"/>
+                <input 
+                onKeyPress={(e:any) => {if (e.code === "Enter") {load(e.target.value)}}} 
+                type="text"
+                placeholder="Tente 'Charizard'"
+                />
             </div>
-            <div className={styles.pokemon}>
-                <div className={styles.heading}>
-                    <p>{displayPokemon?.name}</p>
-                    <p>Id: {displayPokemon?.id}</p>
-                </div>
-                <div className={styles.image}>
-                    <img src={displayPokemon?.sprites.front_default} alt=""/>
-                    <img src={displayPokemon?.sprites.back_default} alt=""/>
-                </div>
-                <div className={styles.text}>
-                    <div className={styles.types}>
-                        <p id={styles.typesHeading}><strong>Tipos</strong></p>
-                        <p></p>
-                        <div className={styles.typesContent}>
-                            {displayPokemon?.types ? displayPokemon.types.map(type => {return (
-                                <p className="pokemonType">{type.type.name}</p>
-                            )}):null}
+            {displayPokemon ? (
+                <div className={styles.pokemon}>
+                    <div className={styles.heading}>
+                        <p>{capitalizeFirstLetter(displayPokemon?.name)}</p>
+                        <p>Id: {displayPokemon?.id}</p>
+                    </div>
+                    <div className={styles.image}>
+                        <img src={displayPokemon?.sprites.front_default} alt=""/>
+                        <img src={displayPokemon?.sprites.back_default} alt=""/>
+                    </div>
+                    <div className={styles.text}>
+                        <div className={styles.types}>
+                            <p id={styles.typesHeading}><strong>Tipos</strong></p>
+                            <p></p>
+                            <div className={styles.typesContent}>
+                                
+                                {displayPokemon?.types ? displayPokemon.types.map(type => {return (
+                                    <>
+                                        <img className={styles.icons} src={icons[type.type.name]} alt="icons"/>
+                                        <p className="pokemonType">{capitalizeFirstLetter(type.type.name)}</p>
+                                    </>
+                                )}):null}
+                            </div>
+                        </div>
+                        <div className={styles.stats}>
+                            <p id={styles.statsHeading}><strong>Stats</strong></p>
+                            <p></p>
+                            <div className={styles.statsContent}>
+                                <p><strong>Peso: </strong> {displayPokemon ? displayPokemon.weight / 10 : null} Kg</p>
+                                <p><strong>Altura: </strong> {displayPokemon ? displayPokemon.height / 10 : null} m</p>
+                                {displayPokemon?.stats ? displayPokemon.stats.map(stats => {return (
+                                    <p><strong>{stats.stat.name}: </strong>{stats.base_stat}</p>
+                                )}):null}
+                            </div>
                         </div>
                     </div>
-                    <div className={styles.stats}>
-                        <p id={styles.statsHeading}><strong>Stats</strong></p>
-                        <p></p>
-                        <div className={styles.statsContent}>
-                            <p><strong>Weight: </strong> {displayPokemon ? displayPokemon.weight / 10 : null} Kg</p>
-                            <p><strong>Height: </strong> {displayPokemon ? displayPokemon.height / 10 : null} m</p>
-                            {displayPokemon?.stats ? displayPokemon.stats.map(stats => {return (
-                                <p><strong>{stats.stat.name}: </strong>{stats.base_stat}</p>
-                            )}):null}
-                        </div>
-                    </div>
                 </div>
-            </div>
-
+            ) : (
+                <>
+                </>
+            )}
         </div>
     )
 }
